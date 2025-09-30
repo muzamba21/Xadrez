b2c55da8cf84a373b72a1de798fc6662cab4db09
muzamba21 <68131169+muzamba21@users.noreply.github.com>
2025-09-30 20:54:05 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,4 +1,5 @@
-// Desafio Nivel Novato - Movimento de Peças
+// Desafio Nivel Aventureiro - Movimento de Peças
+// Aluno: [Coloque seu nome aqui]
 
 #include <stdio.h>
 
@@ -7,7 +8,10 @@ int main() {
     int casasDaTorre = 5;
     int casasDoBispo = 5;
     int casasDaRainha = 8;
-    int cont; // Um contador que vou usar nos loops
+
+    // Variáveis que vamos usar nos loops
+    int cont;
+    int passosDados;
 
     // 1. Movimento da Torre com o loop FOR
     printf("--- Movimento da Torre ---\n");
@@ -37,6 +41,42 @@ int main() {
         // Tenho que lembrar de incrementar o contador, senão o loop fica infinito
         cont++;
     }
+
+    printf("\n"); // Espaço antes da nova peça
+
+
+    // 4. Movimento do Cavalo com LOOPS ANINHADOS
+    // O cavalo se move 2 casas para baixo e 1 para a esquerda.
+    printf("--- Movimento do Cavalo ---\n");
+
+    int movVertical = 2;
+    int movHorizontal = 1;
+
+    // O loop FOR de fora vai controlar as duas FASES do movimento em "L":
+    // Fase 1: Movimento vertical.
+    // Fase 2: Movimento horizontal.
+    for (int fase = 1; fase <= 2; fase++) {
+
+        // Se for a primeira fase, faz o movimento vertical
+        if (fase == 1) {
+            passosDados = 0; // Zera o contador de passos
+            // Este loop WHILE (aninhado) executa os passos para baixo
+            while (passosDados < movVertical) {
+                printf("Baixo\n");
+                passosDados++;
+            }
+        }
+        // Se for a segunda fase, faz o movimento horizontal
+        else {
+            passosDados = 0; // Zera de novo
+            // Este loop WHILE (aninhado) executa o passo para a esquerda
+            while (passosDados < movHorizontal) {
+                printf("Esquerda\n");
+                passosDados++;
+            }
+        }
+    }
+
 
     return 0;
 }
